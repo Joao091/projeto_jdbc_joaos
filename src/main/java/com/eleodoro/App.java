f553ce6ec9fec6facc19d574c56cde8d689b1a26
Joao091 <149343794+Joao091@users.noreply.github.com>
2024-07-02 23:30:37 -0300

    Update App.java

--- src/main/java/com/eleodoro/App.java
+++ src/main/java/com/eleodoro/App.java
@@ -1,13 +1,23 @@
 package com.eleodoro;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Connection conexao = new Conexao().getConnection();
+        TurmaDao turmaDao = new TurmaDao(conexao);
+
+        Turma turmaInsert = new Turma("testeTurmainsert", "654321");
+        Turma turmaInserida = turmaDaoInsert(turmaInsert);
+
+        Turma turmaSelecionada = turmaDao.selectPorId(turmaInserida);
+
+        turmaDao.delete(turmaSelecionada);
+
+        ArrayList<Turma> turmas = turmaDao.selectAll();
+
+        Turma turmaUpdate = new turma(1, "turma3");
+        Turma turmaAlterada = turmaDaoUpdate(turmaUpdate);
     }
 }
